52f869fe1693c52cbce55d0ee6b1fa2867e0d99d
Mikkel Hoegh <mikkel@hoegh.org>
2014-11-03 20:49:15 +0100

    Refactored protocol selector to url-input.
    Now also contains the URL field itself.

--- tests/unit/components/url-input-test.js
+++ tests/unit/components/url-input-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForComponent('protocol-selector', 'ProtocolSelectorComponent', {
+moduleForComponent('url-input', 'UrlInputComponent', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
