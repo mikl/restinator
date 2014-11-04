f05d57cfd809938f8bc1a179e522bcfaba8b22a9
Mikkel Hoegh <mikkel@hoegh.org>
2014-11-04 22:57:43 +0100

    Add default headers on new request route.

--- app/models/request.js
+++ app/models/request.js
@@ -1,9 +1,25 @@
 import DS from 'ember-data';
 
+var defaultHeaders = {
+  Accept: 'application/json',
+  'Accept-Charset': 'utf-8',
+  'Content-Type': 'application/json',
+};
+
 export default DS.Model.extend({
   protocol: DS.attr('string', {
     defaultValue: 'http',
   }),
   url: DS.attr('string'),
-  headers: DS.hasMany('request-header')
+  headers: DS.hasMany('request-header'),
+
+  addDefaultHeaders: function () {
+    Object.keys(defaultHeaders).forEach(function (name) {
+      this.store.createRecord('request-header', {
+        name: name,
+        value: defaultHeaders[name],
+        request: this
+      });
+    }, this);
+  }
 });
--- app/routes/request.js
+++ app/routes/request.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return this.store.createRecord('request');
+    var model = this.store.createRecord('request');
+
+    // When generating a new request for editing, add the default
+    // headers, so we don't start from scratch every time.
+    model.addDefaultHeaders();
+
+    return model;
   }
 });
